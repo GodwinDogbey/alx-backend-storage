d2f9a448d3f3a9c7b22774814bf09b16dbd3d5b5
GodwinDogbey <godwindogbey95@gmail.com>
2024-01-23 22:48:56 +0000

    update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
-""" Provides stats about Nginx logs restored in mongoDB"""
-import pymongo as pm
-db = pm.MongoClient()
-mydb = db["logs"]
-mycol = mydb["nginx"]
+"""
+script that provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    get_get = mycol.count_documents({"method": "GET"})
-    get_post = mycol.count_documents({"method": "POST"})
-    get_put = mycol.count_documents({"method": "PUT"})
-    get_patch = mycol.count_documents({"method": "PATCH"})
-    get_delete = mycol.count_documents({"method": "DELETE"})
-    get_total = mycol.count_documents({})
-    get_status = mycol.count_documents({"method": "GET", "path": "/status"})
-
-    print("{} logs".format(get_total))
-    print("Methods:\n\tmethod GET: {}\n\tmethod POST: {}\n\tmethod PUT: {}\n\tmethod PATCH: {}\n\tmethod DELETE: {}".format(
-        get_get, get_post, get_put, get_patch, get_delete))
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+    # get number of documents in collection
+    docs_num = nginx_logs.count_documents({})
+    get_num = nginx_logs.count_documents({'method': 'GET'})
+    post_num = nginx_logs.count_documents({'method': 'POST'})
+    put_num = nginx_logs.count_documents({'method': 'PUT'})
+    patch_num = nginx_logs.count_documents({'method': 'PATCH'})
+    delete_num = nginx_logs.count_documents({'method': 'DELETE'})
+    get_status = nginx_logs.count_documents({'method': 'GET',
+                                             'path': '/status'})
+    IPs_count = nginx_logs.aggregate([
+        {
+            '$group': {
+                '_id': "$ip",
+                'count': {'$sum': 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        }
+    ])
+    print("{} logs".format(docs_num))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_num))
+    print("\tmethod POST: {}".format(post_num))
+    print("\tmethod PUT: {}".format(put_num))
+    print("\tmethod PATCH: {}".format(patch_num))
+    print("\tmethod DELETE: {}".format(delete_num))
     print("{} status check".format(get_status))
-
+    print("IPs:")
+    x = 0
+    for i in IPs_count:
+        print("\t{}: {}".format(i.get('_id'), i.get('count')))
+        x += 1
+        if x > 9:
+            break
